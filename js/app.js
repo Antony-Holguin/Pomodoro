e3ee80fe19b22aafa27ad012c888129355a61a79
Antony-Holguin <antonyholguin37@gmail.com>
2023-02-10 17:24:54 -0500

    Se agrego funciones en css

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
 document.addEventListener('DOMContentLoaded', iniciarApp);
 
-
-
+const tareas = [];
+const boton = document.querySelector('#iniciar').addEventListener('click', mostrarTarea);
 
 function iniciarApp(){
-    
+    mostrarTarea();
 }
 
+function mostrarTarea(){
+    const tarea = document.getElementById('input-texto').value;
+    tareas.unshift(tarea);
+    const contenedorPrincipal = document.querySelector('.contenedor');
+    const elemento = document.createElement('DIV');
+    elemento.classList.add('parafo');
+    elemento.innerHTML = `<p>${tarea}</p>`;
+    contenedorPrincipal.appendChild(elemento);
+
+    setTimeout(elemento.classList.remove(),5000);
+}
